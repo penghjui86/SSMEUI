7097ee67224db72e2f34965a8c03b2b076a74c6f
Administrator <121206324@qq.com>
2017-10-09 07:29:19 +0800

    组织机构

--- src/main/java/com/fceg/core/dao/SsmRoleMapper.java
+++ src/main/java/com/fceg/core/dao/SsmRoleMapper.java
@@ -1,9 +1,12 @@
 package com.fceg.core.dao;
 
 import com.fceg.core.domain.SsmRole;
-import com.fceg.util.IBaseMapper;
-import org.springframework.stereotype.Repository;
+import com.fceg.core.domain.SsmRoleExample;
+import java.util.List;
 
+import com.fceg.util.IBaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository("ssmRoleMapper")
 public interface SsmRoleMapper extends IBaseMapper<SsmRole> {
--- src/main/webapp/jsp/org/ssm-org.js
+++ src/main/webapp/jsp/org/ssm-org.js
@@ -1,3 +1,51 @@
-/*
-//阿什顿发斯蒂芬
-//阿斯蒂芬*/
+var ctx="";//项目部署路径
+var SsmOrgList;
+var SsmOrgEdit;
+var SsmOrgForm;
+
+var SsmOrg={
+    URL:{
+        inputUI:function () {
+            return ctx+"/org/ui/input";
+        },
+        listUI:function () {
+            return ctx+  "/org/ui/list";
+        },
+        list:function () {
+            return ctx+"/org/list";
+        },
+        tree:function () {
+            return ctx+"/org/tree":
+        },
+        save:function () {
+            return ctx+"/org/save";
+        },
+        delete:function (ids) {
+            return ctx+"/org/delete/"+ids;
+        },
+        get:function (id) {
+            return ctx+"/org/get/"+id;
+        }
+    },
+    input:{
+        init:function () {
+
+        },
+        initComponent:function () {
+
+        },
+        initForm:function () {
+
+        },
+        submitForm:function () {
+
+        },
+        close:function () {
+
+        }
+    },
+    list:{
+
+    }
+};
+
